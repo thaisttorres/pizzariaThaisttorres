e78f246c2d94c6d63ea6f05a614efca8e8eb4b8d
Thaisttorres <thaisttorres1991@gmail.com>
2023-05-26 12:26:20 -0300

    pizzariaThaisttorres

---
+++ pizzas.js
@@ -0,0 +1,9 @@
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'images/pizza.png', price:30.99, sizes:['100g', '530g', '860g'], description:'Pizza sabor quejo mussarela'},
+    {id:2, name:'Marguerita', img:'images/pizza2.png', price:38.99, sizes:['320g', '530g', '860g'], description:'Pizza com queijo Mussarela, manjericao e tomate'},
+    {id:3, name:'Pimentao', img:'images/pizza3.png', price:37.45, sizes:['320g', '530g', '860g'], description:'Pizza com mussarela, pimentao verde, vermelho e amarelo'},
+    {id:4, name:'Brocolis com Queijo', img:'images/pizza4.png', price:19.77, sizes:['320g', '530g', '860g'], description:'Pizza com base de mussarela, tomate e brocolis'},
+    {id:5, name:'Americana', img:'images/pizza5.png', price:41.43, sizes:['320g', '530g', '860g'], description:'Pizza com base de mussarela, salame e pimentao'},
+    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:48.55, sizes:['320g', '530g', '860g'], description:'Pizza com mussarela, catupiry, tomate, azeitona, manjericao e cheddar'},
+    {id:7, name:'Chocolate com M&MS', img:'images/pizza7.png', price:42.36, sizes:['320g', '530g', '860g'], description:'Base de chocoalte ao leite com M&MS por cima'}
+];
